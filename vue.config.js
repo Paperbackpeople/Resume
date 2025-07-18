22500e9c6307ebab7a540a88972bd5dbc14543b7
Zhaoyu Wang <1241077523@qq.com>
2025-07-18 09:47:12 +0000

    change profile

--- vue.config.js
+++ vue.config.js
@@ -11,13 +11,3 @@ module.exports = defineConfig({
     ]
   }
 })
-// const { defineConfig } = require('@vue/cli-service')
-
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   configureWebpack: {
-//     define: {
-//       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
-//     }
-//   }
-// })
